105cd91d6f0e804536420d7f95a06cf0c8178f41
Темирбулат Ануар Жанабекович <@corp.itstep>
2018-12-05 18:40:25 +0600

    asd

---
+++ EntityFrameWork/Data/ShopContext.cs
@@ -0,0 +1,32 @@
+﻿namespace EntityFrameWork.Data
+{
+    using EntityFrameWork.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ShopContext : DbContext
+    {
+        // Контекст настроен для использования строки подключения "ShopContext" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "EntityFrameWork.Data.ShopContext" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "ShopContext" 
+        // в файле конфигурации приложения.
+        public ShopContext()
+            : base("name=ShopContext")
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ EntityFrameWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using EntityFrameWork.Data;
+using EntityFrameWork.Models;
+using System.Collections.Generic;
+using System.Linq;
+namespace EntityFrameWork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICollection<Item> items = null;
+            using(var context=new ShopContext())
+            {
+               items=context.Items.ToList();
+                context.Items.Add(new Item
+                {
+                    Name = "tea",
+                    Price = 400
+                });
+                context.SaveChanges();
+            }
+            foreach(var item in items)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
